5dfb6a9a797d709d035537d41463fc258194f27a
Aazim Anish <aazimanish1@gmail.com>
2024-12-01 23:27:22 +0530

    feat: okto login button integrated to frontend

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,3 +1,5 @@
+// okto login with google button
+
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
@@ -11,11 +13,11 @@ export function LoginButton() {
 
   return (
     <button
-      className={`border border-transparent rounded px-4 py-2 transition-colors ${
-        session
-          ? "bg-red-500 hover:bg-red-700 text-white"
-          : "bg-blue-500 hover:bg-blue-700 text-white"
-      }`}
+    //   className={`border border-transparent rounded px-4 py-2 transition-colors ${
+    //     session
+    //       ? "bg-red-500 hover:bg-red-700 text-white"
+    //       : "bg-blue-500 hover:bg-blue-700 text-white"
+    //   }`}
       onClick={handleLogin}
     >
       Google {session ? "Log Out" : "Log In"}
